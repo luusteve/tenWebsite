53a1f111630e87c732013ac482c6de6ad33149df
Steven <luu.stevenluu@gmail.com>
2020-06-20 21:02:41 -0400

    Updates

---
+++ precache-manifest.2ccbde729f57995ecd618d829a73f981.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496bf03df1be571efb913524e8880e77",
+    "url": "/tenWebsite/index.html"
+  },
+  {
+    "revision": "b2f1b34c90480cb717ac",
+    "url": "/tenWebsite/static/css/main.34dc3d58.chunk.css"
+  },
+  {
+    "revision": "ec6cfc53945c84d0b61d",
+    "url": "/tenWebsite/static/js/2.b7167481.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tenWebsite/static/js/2.b7167481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f1b34c90480cb717ac",
+    "url": "/tenWebsite/static/js/main.394cfc7c.chunk.js"
+  },
+  {
+    "revision": "fd2bc1fbcea2e9f21c28",
+    "url": "/tenWebsite/static/js/runtime-main.e5c41cf2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tenWebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
